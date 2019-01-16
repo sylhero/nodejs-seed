a8f161b3b608d0a4a0c71e2a5f50a0a7d7ab56c3
yulong song <yulongsong@yulongs-MacBook-Pro.local>
2019-01-15 22:43:12 -0500

    switched to hapi

--- src/Server.ts
+++ src/Server.ts
@@ -1,20 +1,33 @@
-import * as Koa from 'koa';
-
-import Middlewares from './Middlewares';
-import Routes from './Routes';
+import * as Hapi from 'hapi';
+import { ApolloServer, gql } from 'apollo-server-hapi';
 
 const config = {
     host: process.env.host || 'localhost',
     port: process.env.port || 3000
 };
 
-const app = new Koa();
-app.use(Middlewares);
-app.use(Routes);
+const init = async (): Promise<any> => {
+    const server = new ApolloServer({});
+    const app = new Hapi.Server({
+        ...config,
+        debug: { request: ['error'] },
+        routes: {
+            cors: true
+        }
+    });
+    await server.applyMiddleware({
+        app
+    });
+    await server.installSubscriptionHandlers(app.listener);
+    await app.start();
+};
 
-const Server = app.listen(config);
-
-/* tslint:disable:no-console */
-console.log(`Server running on host ${config.host} port ${config.port}`);
-
-export default Server;
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+try {
+    init();
+} catch (error) {
+    console.log(error);
+}
