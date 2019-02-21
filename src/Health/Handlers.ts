5a9aa2aa960e03480394e868d6db7bc1bb23a098
Yulong Song <yulong.song@ormuco.com>
2019-02-21 17:14:53 -0500

    handler test

--- src/Health/Handlers.ts
+++ src/Health/Handlers.ts
@@ -6,10 +6,8 @@ interface HealthPost {
     age: number;
 }
 const healthPostHandler = async (request: any): Promise<HealthPost> => {
-    request.logger.info('In handler %s', request.path);
-    request.logger.info('request', request);
     return {
-        name: 'fdfdf',
+        name: request.params.name,
         age: 12
     };
 };
